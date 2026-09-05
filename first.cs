7d688b7b0ab5f99b08b371cee926053fc021719c
Sayem Ahmed Sayem <sayemahmed92@gmail.com>
2026-09-06 00:38:22 +0600

    Update first.css

--- first.cs
+++ first.cs
@@ -6,7 +6,9 @@ namespace HelloWorld
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");    
+      Console.WriteLine("Hello World!");
+	  Console.WriteLine("I am Learning C#");
+	  Console.WriteLine("It is awesome!");   
     }
   }
 }
