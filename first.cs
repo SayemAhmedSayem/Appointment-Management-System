ef53c558479c429064c3b1f5e133e9d9773d77bf
Sayem Ahmed Sayem <sayemahmed92@gmail.com>
2026-09-17 17:41:17 +0600

    Update First.cs
    Multi-line comments start with /* and ends with */.
    
    Any text between /* and */ will be ignored by C#.
    
    This example uses a multi-line comment (a comment block) to explain the code:

--- first.cs
+++ first.cs
@@ -15,6 +15,10 @@ namespace HelloWorld
 	  // This is a comment
 	  Console.WriteLine("Hello World!");
 	  Console.WriteLine("Hello World!");  // This is a comment
+	  
+	  /* The code below will print the words Hello World
+		to the screen, and it is amazing */
+		Console.WriteLine("Hello World!"); 
     }
   }
 }
